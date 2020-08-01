fe1a2ea0d1b71e9c55c7cac1115f8024e974086c
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-08-01 17:29:28 +0530

    added battle  instructions

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Popular from "./components/Popular";
+import Battle from "./components/Battle";
 import "./index.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Popular />
+        <Battle />
       </div>
     );
   }
