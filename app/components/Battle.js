4572b81170b95d2a9474a02dacf3aaaa40c7f960
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-08-01 18:44:52 +0530

    added player input form

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
 
 function Instructions() {
@@ -26,12 +27,69 @@ function Instructions() {
     </div>
   );
 }
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      username: "",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.username);
+  }
+  handleChange(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+  render() {
+    return (
+      <form className="column player" onSubmit={this.handleSubmit}>
+        <label htmlFor="username" className="player-label">
+          {this.props.label}
+        </label>
+        <div className="row player-inputs">
+          <input
+            type="text"
+            id="username"
+            className="input-light"
+            placeholder="github username"
+            autoComplete="off"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button
+            className="btn dark-btn"
+            type="submit"
+            disabled={!this.state.username}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
 export default class Battle extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Instructions />
+        <PlayerInput
+          label="User Name"
+          onSubmit={(value) => console.log(value)}
+        />
       </React.Fragment>
     );
   }
