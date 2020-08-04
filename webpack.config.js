99f01b327600e7698d98b18f18f601834fa96b2e
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-08-05 00:49:04 +0530

    hostiing with netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./app/index.js",
@@ -14,11 +15,12 @@ module.exports = {
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
     ],
   },
-  mode: "development",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   plugins: [
     new HtmlWebpackPlugin({
       template: "app/index.html",
     }),
+    new CopyPlugin({ patterns: [{ from: "_redirects" }] }),
   ],
   devServer: {
     historyApiFallback: true,
