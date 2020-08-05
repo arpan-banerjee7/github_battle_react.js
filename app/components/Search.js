588e59aa0416e8837dd80947395c11e7b5bec264
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-08-05 18:32:05 +0530

    added reset button for invalid usernames in search page

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { getSingleUser } from "../utils/api";
 import { ThemeConsumer } from "../contexts/theme";
-
+import Loading from "./Loading";
 import {
+  FaStar,
+  FaCodeBranch,
   FaCompass,
-  FaBriefcase,
-  FaUsers,
-  FaUserFriends,
   FaCode,
   FaUser,
+  FaBriefcase,
+  FaUserFriends,
+  FaUsers,
 } from "react-icons/fa";
 import Tooltip from "./Tooltip";
 
@@ -19,6 +21,58 @@ const styles = {
     padding: "10px",
   },
 };
+
+function ReposGrid({ repos }) {
+  return (
+    <ul className="grid space-around">
+      {repos.map((repo, index) => {
+        const {
+          language,
+          description,
+          stargazers_count,
+          forks,
+          full_name,
+          svn_url,
+        } = repo;
+
+        return (
+          <ThemeConsumer>
+            {({ theme }) => (
+              <li key={index}>
+                <div className={`card bg-${theme}`}>
+                  <h4 className="header-lg center-text">{full_name}</h4>
+                  {description && <p className="center-text">{description}</p>}
+                  <h2 className="center-text">
+                    <a className="link" href={svn_url}>
+                      {full_name}
+                    </a>
+                  </h2>
+                  <ul className="card-list">
+                    <li>
+                      <Tooltip text="Stars">
+                        <FaStar color="rgb(255, 215, 0)" size={22} />
+                        {stargazers_count.toLocaleString()} stars
+                      </Tooltip>
+                    </li>
+                    <li>
+                      <FaCode color="rgb(255, 215, 0)" size={22} />
+                      {language}
+                    </li>
+                    <li>
+                      <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
+                      {forks} forks
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            )}
+          </ThemeConsumer>
+        );
+      })}
+    </ul>
+  );
+}
+
 class PlayerSearch extends React.Component {
   constructor(props) {
     super(props);
@@ -84,20 +138,25 @@ export default class Search extends React.Component {
       userName: null,
       profile: null,
       repos: null,
+      loading: false,
       error: null,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleReset = this.handleReset.bind(this);
   }
   handleSubmit(user) {
+    this.setState({
+      loading: true,
+    });
     getSingleUser(user)
       .then(([profile, repos]) => {
         this.setState({
           userName: user,
           profile,
           repos,
+          loading: false,
         });
-        console.log(profile);
+        console.log(repos);
       })
       .catch(({ message }) => {
         this.setState({
@@ -108,77 +167,107 @@ export default class Search extends React.Component {
   }
   handleReset() {
     this.setState({
-      playerName: null,
+      userName: null,
+      profile: null,
+      repos: null,
+      loading: false,
+      error: null,
     });
   }
   render() {
-    const { userName, profile, repos } = this.state;
+    const { userName, profile, repos, loading, error } = this.state;
 
+    if (loading === true) {
+      return <Loading text="Fetching user data..." />;
+    }
+    if (error) {
+      return (
+        <React.Fragment>
+          <p className="center-text error">{error}</p>
+          <div>
+            <button
+              className="btn dark-btn btn-space"
+              onClick={this.handleReset}
+            >
+              Reset
+            </button>
+          </div>
+        </React.Fragment>
+      );
+    }
     return (
       <React.Fragment>
         <div style={styles.center}>
-          <PlayerSearch
-            label="User Name"
-            onSubmit={(player) => this.handleSubmit(player)}
-          />
+          {userName === null && (
+            <PlayerSearch
+              label="User Name"
+              onSubmit={(player) => this.handleSubmit(player)}
+            />
+          )}
 
           <div>
             {profile && repos && (
               <ThemeConsumer>
                 {({ theme }) => (
-                  <div className={`card-search bg-${theme}`}>
-                    <h4 className="header-lg center-text">User Details</h4>
-                    <img
-                      className="avatar-search"
-                      src={profile.avatar_url}
-                      alt={`Avatar for ${userName}`}
-                    />
-                    <h2 className="center-text">
-                      <a className="link" href={profile.html_url}>
-                        {profile.name}
-                      </a>
-                    </h2>
-                    <div style={styles.center}>
-                      <ul className="card-list-search">
-                        <li>
-                          <FaUser color="rgb(239, 115, 115)" size={22} />
-                          {profile.login}
-                        </li>
-                        {profile.location && (
+                  <React.Fragment>
+                    <div className={`card-search bg-${theme}`}>
+                      <h4 className="header-lg center-text">User Details</h4>
+                      <img
+                        className="avatar-search"
+                        src={profile.avatar_url}
+                        alt={`Avatar for ${userName}`}
+                      />
+                      <h2 className="center-text">
+                        <a className="link" href={profile.html_url}>
+                          {profile.name}
+                        </a>
+                      </h2>
+                      <div style={styles.center}>
+                        <ul className="card-list-search">
                           <li>
-                            <Tooltip text="User's location">
-                              <FaCompass color="rgb(144, 115, 255)" size={22} />
-                              {profile.location}
-                            </Tooltip>
+                            <FaUser color="rgb(239, 115, 115)" size={22} />
+                            {profile.login}
                           </li>
-                        )}
-                        {profile.company && (
+                          {profile.location && (
+                            <li>
+                              <Tooltip text="User's location">
+                                <FaCompass
+                                  color="rgb(144, 115, 255)"
+                                  size={22}
+                                />
+                                {profile.location}
+                              </Tooltip>
+                            </li>
+                          )}
+                          {profile.company && (
+                            <li>
+                              <Tooltip text="User's company">
+                                <FaBriefcase color="#795548" size={22} />
+                                {profile.company}
+                              </Tooltip>
+                            </li>
+                          )}
+                          {profile.public_repos && (
+                            <li>
+                              <Tooltip text="Public Repos">
+                                <FaCode color="#795548" size={22} />
+                                {profile.public_repos} repositories
+                              </Tooltip>
+                            </li>
+                          )}
                           <li>
-                            <Tooltip text="User's company">
-                              <FaBriefcase color="#795548" size={22} />
-                              {profile.company}
-                            </Tooltip>
+                            <FaUsers color="rgb(129, 195, 245)" size={22} />
+                            {profile.followers.toLocaleString()} followers
                           </li>
-                        )}
-                        {profile.public_repos && (
                           <li>
-                            <Tooltip text="Public Repos">
-                              <FaCode color="#795548" size={22} />
-                              {profile.public_repos} repositories
-                            </Tooltip>
+                            <FaUserFriends color="rgb(64, 183, 95)" size={22} />
+                            {profile.following.toLocaleString()} following
                           </li>
-                        )}
-                        <li>
-                          <FaUsers color="rgb(129, 195, 245)" size={22} />
-                          {profile.followers.toLocaleString()} followers
-                        </li>
-                        <li>
-                          <FaUserFriends color="rgb(64, 183, 95)" size={22} />
-                          {profile.following.toLocaleString()} following
-                        </li>
-                      </ul>
+                        </ul>
+                      </div>
                     </div>
-                  </div>
+                    {/* <ReposGrid repos={repos} /> */}
+                  </React.Fragment>
                 )}
               </ThemeConsumer>
             )}
